29d7b1945f22c6bc54beaef974bdf1e3fd19ecf7
tartamudeando1 <135078057+tartamudeando1@users.noreply.github.com>
2024-09-27 11:59:57 -0300

    .

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function areaQuadrado(num1){
 }
 console.log(areaQuadrado(4));
 
-let media = function(n1, n2, n3, n4){
-return n1+n2+n3/n4
+let media = function(n1, n2, n3){
+return (n1+n2+n3)/3
 }
-console.log(media(1,2,3,3));
+console.log(media(1,2,3));
