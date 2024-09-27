99b8d46d81387895baadf709125747693d982a16
tartamudeando1 <135078057+tartamudeando1@users.noreply.github.com>
2024-09-27 12:33:56 -0300

    .

--- main.js
+++ main.js
@@ -17,15 +17,17 @@ function areaQuadrado(num1){
 }
 console.log(areaQuadrado(4));
 
+let conta1 = parseInt(prompt('qual sera o = n1'));
+let conta2 = parseInt(prompt('qual sera o = n2'));
+let conta3 = parseInt(prompt('qual sera o = n3'));
 
-let conta = parseInt(prompt('qual sera o = n1, n2, n3'));
 let media = function(n1, n2, n3){ 
 
     return (n1+n2+n3)/3
     
     }
     
-    document.write(conta,media)
+    document.write(conta1,conta2,conta3)
 
 
 
