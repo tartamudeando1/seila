20225863626ecebc29c10a67c3dc3d28ee788104
tartamudeando1 <135078057+tartamudeando1@users.noreply.github.com>
2024-10-04 11:20:51 -0300

    .

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-function soma(num1, num2){
-    return num1+num2;
-}
-console.log(soma(6,4));
+// function soma(num1, num2){
+    // return num1+num2;
+// }
+// console.log(soma(6,4));
 
 
 
-function subi(num1, num2){
-    return num1-num2;
-}
-console.log(subi(16,6));
+// function subi(num1, num2){
+    // return num1-num2;
+// }
+// console.log(subi(16,6));
 
 
 
-function areaQuadrado(num1){
-    return num1**2;
-}
-console.log(areaQuadrado(4));
+// function areaQuadrado(num1){
+    // return num1**2;
+// }
+// console.log(areaQuadrado(4));
 
-let conta1 = parseInt(prompt('qual sera o = n1'));
-let conta2 = parseInt(prompt('qual sera o = n2'));
-let conta3 = parseInt(prompt('qual sera o = n3'));
+// let conta1 = parseInt(prompt('qual sera o = n1'));
+// let conta2 = parseInt(prompt('qual sera o = n2'));
+// let conta3 = parseInt(prompt('qual sera o = n3'));
 
-let media = function(n1, n2, n3){ 
+// let media = function(n1, n2, n3){ 
 
-    return (n1+n2+n3)/3
+    // return (n1+n2+n3)/3
     
-    }
+    // }
     
-    document.write(media(conta1,conta2,conta3))
+    // document.write(media(conta1,conta2,conta3))
 
 
 
 //console.log(media(1,2,3));
+
+let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
+
