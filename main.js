26ef6c6474a8cb63797f8456fb4015fdbc0864ff
tartamudeando1 <135078057+tartamudeando1@users.noreply.github.com>
2024-10-04 11:44:38 -0300

    .

--- main.js
+++ main.js
@@ -35,7 +35,7 @@
 
 let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
    let i =0;
-     while(i < 10){
+     while(i <= 10){
         console.log(i);
     i = i+1;    
 }
